1c97b6a7559806366d27eb95becc218c6b6945a3
Aperitas <dbowen2024@163.com>
2026-08-16 22:09:49 +0800

    FastAPI后端：ROS小车位置上报接口 + 前端车辆实时查询接口

--- agv_back_fastapi/register/router.py
+++ agv_back_fastapi/register/router.py
@@ -13,7 +13,7 @@ from apis.admin import admin_api
 from apis.admin.sites import router as sites_router
 from apis.admin.waypoints import router as waypoints_router
 from apis.admin.paths import router as paths_router
-
+from apis.client.report import router as report_router
 def register_router(app: FastAPI):
     """ 注册路由 """
 
@@ -23,6 +23,7 @@ def register_router(app: FastAPI):
     app.include_router(oauth_api, prefix=settings.API_PREFIX, tags=["Oauth"])  # Login(权限在每个接口上)
     app.include_router(commons_api, prefix=settings.API_PREFIX, tags=["Common"])  # Login(权限在每个接口上)
     app.include_router(client_api,prefix=settings.API_PREFIX,tags=['Client'])
+    app.include_router(report_router)  # 车端上报接口
     app.include_router(admin_api,prefix=settings.API_PREFIX,tags=['Admin'])
     app.include_router(websocket_api,tags=['Websocket'])
     app.include_router(sites_router)
